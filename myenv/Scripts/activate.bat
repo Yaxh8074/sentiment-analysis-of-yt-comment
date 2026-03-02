6c9a44870fde7e6f279d38a2ade84f7448418306
Yaxh8074 <yashzanwar74@gmail.com>
2026-03-02 18:36:21 +0530

    Refactor code structure for improved readability and maintainability

--- myenv/Scripts/activate.bat
+++ myenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Nitish\Desktop\yt-comment-sentiment-analysis\myenv
+set VIRTUAL_ENV=C:\Users\krish\OneDrive\Desktop\Youtube sentiment analysis project\sentiment-analysis-of-yt-comment\myenv
 
 if not defined PROMPT set PROMPT=$P$G
 
